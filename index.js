c3cf96fcd42e935e8ca54a4370efe1215f74c385
Maxime Kermarrec <m.kermarrec@s2fnetwork.fr>
2018-11-02 11:44:10 +0100

    Ajout d'une fonction de recuperation des donnees non testables pour le moment

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express')
+const express = require('express');
+var bodyParser = require("body-parser");
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+var server = express();
+	server.use(bodyParser.urlencoded({ extended: true }))
 	.use(express.static(path.join(__dirname, 'views')))
 	.set('js', path.join(__dirname, 'js'))
   	.set('view engine', 'ejs')
 	.get('/', (req, res) => res.render('pages/index'))
-  	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+server.post('/users', function(request, response) {
+	var data = JSON.stringify(request.body); //Doit contenir les logs recues depuis thingpark
+	console.log(data);
+});
